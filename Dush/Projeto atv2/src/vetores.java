f1f3ac5c194729f21c1c8c86c98d241b895698ea
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-13 20:46:58 -0300

    feat: Projeto atv2

---
+++ Dush/Projeto atv2/src/vetores.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class main {
+    public static void main(String[] args) {
+        ArrayList<Double> notas = new ArrayList<>();
+
+        notas.add(10.0);
+        notas.add(8.0);
+
+        for (double nota : notas) {
+            System.out.println("Nota: " + nota);
+        }
+    }
+}

7cf94ce025b53efbd247f5c417606a58c3acd2de
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-20 19:50:00 -0300

    feat: compilantoatv5

---
+++ atv5/src/main/java/org/example/Cliente.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Cliente {
+
+    // atributos
+    private String nome;
+    private int idade;
+
+    // construtor
+
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+
+    public Cliente(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
